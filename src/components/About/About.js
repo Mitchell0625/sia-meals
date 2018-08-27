3aa68c161f16bc704ef248d7e5a2b466abae217c
Alexx Mitchell <alexxmitchell25@gmail.com>
2018-08-27 11:44:56 -0400

    working on About, Contact, and Landslide

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './About.css';
 
 const propTypes = {
-  id: PropTypes.number.isRequired,
   text: PropTypes.string.isRequired
 };
 
--- src/components/Landing/LandSlide/LandSlide.js
+++ src/components/Landing/LandSlide/LandSlide.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './LandSlide.css';
 
 const propTypes = {
   url: PropTypes.string.isRequired
